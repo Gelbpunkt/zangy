c4f37510907a2f670b8d662457763f01ae4fa6f8
Jens Reidel <jens@troet.org>
2020-07-12 22:22:45 +0200

    Not working hiredis refs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,37 @@
+use hiredis_sys::*;
 use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
+use pyo3::types::PyBytes;
 
-#[pyfunction]
-fn test() -> String {
-    String::from("hello world")
+#[pyclass(unsendable)]
+struct Reader {
+    __reader: *mut redisReader,
+}
+
+#[pymethods]
+impl Reader {
+    #[new]
+    fn new() -> Self {
+        Reader {
+            __reader: unsafe { redisReaderCreate() },
+        }
+    }
+
+    fn feed(&self, _data: &PyBytes) -> PyResult<()> {
+        let actual_bytes = _data.as_bytes();
+        unsafe {
+            redisReaderFeed(self.__reader, actual_bytes, actual_bytes.len());
+        }
+        Ok(())
+    }
+
+    fn gets(&self) -> PyResult<()> {
+        Ok(())
+    }
 }
 
 #[pymodule]
 fn zangy(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(test))?;
+    m.add_class::<Reader>()?;
 
     Ok(())
 }
