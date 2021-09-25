9c57dea7e320a041c2c42f9ce8afeb7f129e1642
Jens Reidel <adrian@travitia.xyz>
2021-09-25 18:15:08 +0200

    update deps

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export RUSTFLAGS="-C target-cpu=native -Z mir-opt-level=3"
+export RUSTFLAGS="-C target-cpu=native -Z tune-cpu=native -Z mir-opt-level=3"
 maturin build --no-sdist --release --strip --manylinux off --interpreter python3
 pip install target/wheels/*.whl -U --force-reinstall
